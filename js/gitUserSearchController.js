af4ef0699276864e7e48d89b79318891fd2dbeea
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-29 12:15:55 +0100

    Halfway step - get tests to pass but not returning any real data

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -2,11 +2,12 @@ githubUserSearch.controller('GitUserSearchController', ['$resource', function($r
 
   var self = this;
 
-  var searchResource = $resource('https://api.github.com/search/users');
+  var searchResource =  $resource('https://api.github.com/search/users');
 
   self.doSearch = function() {
     self.searchResult = searchResource.get(
-      { q: self.searchTerm }
+      { q: self.searchTerm,
+        access_token: token }
     );
   };
 
